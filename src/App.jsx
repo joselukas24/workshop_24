24a39d3bb035f84ea497f09e50aaf283970b68db
Jose Lopez Renteria <joselukas24@gmail.com>
2023-06-29 16:32:25 -0500

    finished mapping over data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
+import { puppyList } from "./data";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  let newList = puppies.map((puppy) => {
+    return <p key={puppy.id}>{puppy.name}</p>;
+  });
+  console.log(newList);
 
-  return (
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
-  );
+  return <div className="App">{newList}</div>;
 }
 
 export default App;
